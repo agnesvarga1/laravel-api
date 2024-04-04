10acf9aa46887552cd0829b820a62ecdaf30f654
agnesvarga1 <agikolyok@gmail.com>
2024-04-04 15:09:49 +0200

    api endpoint

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+
+
+    public function index(){
+        $projects = Project::All();
+
+        return response()->json([
+            'success'=>true,
+            'projects'=>$projects
+        ]);
+    }
+
+
 }
