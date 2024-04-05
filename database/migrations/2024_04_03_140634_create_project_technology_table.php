ae9c25b5dd5618e5c363d5142cbb771cc445faf6
agnesvarga1 <agikolyok@gmail.com>
2024-04-05 19:54:03 +0200

    primary key

--- database/migrations/2024_04_03_140634_create_project_technology_table.php
+++ database/migrations/2024_04_03_140634_create_project_technology_table.php
@@ -12,12 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('project_technology', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBiginteger('project_id')->nullable();
+            $table->primary(['project_id', 'technology_id']);
+
+            $table->unsignedBiginteger('project_id');
             $table->foreign('project_id')->references('id')->on('projects')->cascadeOnDelete();
-            $table->unsignedBiginteger('technology_id')->nullable();
+            $table->unsignedBiginteger('technology_id');
             $table->foreign('technology_id')->references('id')->on('technologies')->cascadeOnDelete();
-            $table->timestamps();
+
         });
     }
 
